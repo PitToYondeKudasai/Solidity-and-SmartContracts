a7b21772d536122e22b2d2ca43541ed2d5ec8208
PitToYondeKudasai <buttaronipeter@gmail.com>
2020-12-07 13:42:38 +0100

    Create CurrentAccount2.sol

---
+++ CurrentAccount2.sol
@@ -0,0 +1,30 @@
+pragma solidity >= 0.5.0 < 0.7.0;
+
+contract Coin{
+    address public minter;
+    
+    /// We create a mapping for the balances
+    mapping (address => uint) public balances;
+    
+    event Sent(address from, address to, uint amount);
+    
+    /// Constructor method
+    constructor() public{
+        minter = msg.sender;
+    }
+    
+    /// Create a new amopunt of coin on a specific balance
+    function mint(address receiver, uint amount) public {
+        require(msg.sender == minter);
+        require(amount < 1e60);
+        balances[receiver] += amount;
+    }
+    
+    /// Transfer money from an address to another
+    function send(address receiver, uint amount) public{
+        require(amount <= balances[msg.sender], 'Insufficient balance');
+        balances[msg.sender] -= amount;
+        balances[receiver] += amount;
+        emit Sent(msg.sender, receiver, amount);
+    }
+}
